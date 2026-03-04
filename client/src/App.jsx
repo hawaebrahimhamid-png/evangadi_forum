9d31271629df5beb677730db644e2b19e620db43
hawa ebrahim <hawaebrahimhamid@gmail.com>
2026-03-04 14:23:45 +0800

    frontend register page

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/Register.jsx
@@ -0,0 +1,78 @@
+import { useRef } from "react";
+import React from "react";
+import axios from "../AxiosConfig";
+import { useNavigate } from "react-router-dom";
+function Register() {
+  const navigate = useNavigate();
+  const userNameDom = useRef(null);
+  const firstNameDom = useRef(null);
+  const lastNameDom = useRef(null);
+  const emailDom = useRef(null);
+  const passwordDom = useRef(null);
+
+  async function handlSubmit(e) {
+    e.preventDefault();
+    const usernameValue = userNameDom.current.value;
+    const firstValue = firstNameDom.current.value;
+    const lastValue = lastNameDom.current.value;
+    const emailValue = emailDom.current.value;
+    const passwordValue = passwordDom.current.value;
+    if (
+      !usernameValue ||
+      !firstValue ||
+      !lastValue ||
+      !emailValue ||
+      !passwordValue
+    ) {
+      alert("please provide all required information");
+      return;
+    }
+    try {
+      await axios.post("/users/register", {
+        username: usernameValue,
+        firstname: firstValue,
+        lastname: lastValue,
+        email: emailValue,
+        password: passwordValue,
+      });
+      alert("register successfull,please login");
+      navigate("/login");
+    } catch (error) {
+      console.log(error.response);
+      alert(error.response?.data?.msg || error.message);
+    }
+  }
+  return (
+    <section>
+      <form onSubmit={handlSubmit}>
+        <div>
+          <span>username :---</span>
+          <input ref={userNameDom} type="text" placeholder="username" />
+        </div>
+        <br />
+        <div>
+          <span>firstname :---</span>
+          <input ref={firstNameDom} type="text" placeholder="firstname" />
+        </div>
+        <br />
+        <div>
+          <span>lastname :---</span>
+          <input ref={lastNameDom} type="text" placeholder="lastname" />
+        </div>
+        <br />
+        <div>
+          <span>email :---</span>
+          <input ref={emailDom} type="email" placeholder="email" />
+        </div>
+        <br />
+        <div>
+          <span>password :---</span>
+          <input ref={passwordDom} type="password" placeholder="password " />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </section>
+  );
+}
+
+export default Register;
