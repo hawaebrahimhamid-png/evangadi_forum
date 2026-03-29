8f1a65e795db4be8bb1c01480302f1a8dce3747d
hawa ebrahim <hawaebrahimhamid@gmail.com>
2026-03-30 01:44:53 +0800

    fix db config

--- Db/DbConfig.js
+++ Db/DbConfig.js
@@ -1,11 +1,14 @@
 const mysql2 = require("mysql2");
 
 const dbConnection = mysql2.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT, // ✅ VERY IMPORTANT
+  waitForConnections: true,
   connectionLimit: 10,
+  connectTimeout: 10000,
 });
 
 module.exports = dbConnection.promise();
